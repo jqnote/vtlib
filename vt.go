47eea251b8897656ad4a2cbde34a5480050cb037
wjq <disdot@foxmail.com>
2020-01-10 10:57:04 +0800

    update

--- vt.go
+++ vt.go
@@ -1,5 +1,11 @@
 package vt
 
+// #cgo CFLAGS: -O3
+// #define LZ4_DISABLE_DEPRECATE_WARNINGS
+// #include "src/lz4.h"
+// #include "src/lz4.c"
+import "C"
+
 import (
 	"fmt"
 	"github.com/w4n97q/vtlib/src"
