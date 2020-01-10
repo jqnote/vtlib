27c700cad99a5becd6000e21268b68c9b531bc7f
wjq <disdot@foxmail.com>
2020-01-10 10:52:50 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/w4n97q/vtlib
+
+go 1.13
---
+++ vt.go
@@ -0,0 +1,12 @@
+package vt
+
+import (
+	"fmt"
+	"github.com/w4n97q/vtlib/src"
+)
+
+//Show print vtlib name
+func Show() {
+	fmt.Println("i'm " + src.Name)
+	fmt.Println("i'm vtlib")
+}
